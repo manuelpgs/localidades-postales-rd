b36b50e9dcff128db55985017151a4be9bb5513b
Manuel Gonzalez <herohat@gmail.com>
2014-12-14 19:41:41 -0400

    Limpiando los ficheros sql

--- sql/provincias.sql
+++ sql/provincias.sql
@@ -1,8 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.5.8.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
 -- Generation Time: Aug 19, 2013 at 12:42 PM
 -- Server version: 5.5.32-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.2
@@ -15,10 +10,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `app_segurosvivir`
---
 
 -- --------------------------------------------------------
 
